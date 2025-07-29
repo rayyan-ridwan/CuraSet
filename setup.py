87422082e05a6087d87a5f3c5c540ee2458b4ce8
Rayyan Ridwan <rayyan.ridw@gmail.com>
2025-07-29 14:24:20 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='curaset',
+    version='0.1.0',
+    description='A data-centric AI tool for label noise detection and uncertainty scoring',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Rayyan Ridwan',
+    author_email='rayyan.ridw@gmail.com',
+    url='https://github.com/rayyan-ridwan/CuraSet',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+    ],
+    entry_points={
+        'console_scripts': [
+            'curaset=curaset.cli:main',
+        ],
+    },
+    include_package_data=True,
+    python_requires='>=3.7',
+)
